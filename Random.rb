dd8d7ab8b4e56ee87bb9ab1c9f31247cb675f7cb
hausmanne <elie.hausmann@gmail.com>
2020-02-10 16:38:13 +0100

    App code added

--- Random.rb
+++ Random.rb
@@ -2,16 +2,36 @@ class Function
     attr_accessor :apprentices
 
     def addApprentice
-        puts("Veuillez entrer une liste d'apprenti (ex: appr1, appr2, ...)")
+        puts "Veuillez entrer une liste d'apprenti (ex: appr1, appr2, ...)"
         appr = gets
         apprTab = appr.split(", ")
         apprTab.each do |appr|
             apprentices << appr
         end
     end
+
+    def result
+    end
 end
 
 class App
     fonction = Function.new
     fonction.apprentices = Array.new
+    quitter = false
+
+    while quitter==false
+        puts "Veuillez choisir une option: \n1) Ajouter des apprentis\n2) Résultat aléatoire\n3) Quitter"
+        choix = gets
+        case choix
+            when "1\n"
+                fonction.addApprentice
+            when "2\n"
+                fonction.result
+            when "3\n"
+                quitter = true
+            else
+                puts "Mauvais choix"
+        end
+    end
+    puts "Merci d'avoir utilisé notre programme, à bientôt !"
 end
